7756f9a7056f0580bf2cb4b7d49c62d23f15da57
Rafagross <rafagross15@gmail.com>
2024-06-19 12:59:44 -0500

    new-update-of-mu-version

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 35 of the app.' + '\n';
+    response = 'ESTO ES UNA PUTA VERSION PUTAAAAAAAAAAAAAAAAA.' + '\n';
 
     //send the response to the client
     res.send(response);
